b0a92f46dc108628b72460e5d56d2d56985be603
Fernando Serboncini <fserb@fserb.com.br>
2025-02-01 12:30:18 -0500

    split middlewahre and handler code

--- handler.go
+++ handler.go
@@ -7,15 +7,11 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io/fs"
 	"math"
 	"math/big"
-	"net/http"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"sort"
-	"strings"
 	"time"
 
 	"github.com/caddyserver/caddy/v2"
@@ -38,8 +34,8 @@ func init() {
 var (
 	_ caddy.Module                = (*SubstrateHandler)(nil)
 	_ caddy.Provisioner           = (*SubstrateHandler)(nil)
-	_ caddyhttp.MiddlewareHandler = (*SubstrateHandler)(nil)
 	_ caddyfile.Unmarshaler       = (*SubstrateHandler)(nil)
+	_ caddyhttp.MiddlewareHandler = (*SubstrateHandler)(nil)
 )
 
 type Order struct {
@@ -244,75 +240,6 @@ func (s *SubstrateHandler) UpdateOrder(order Order) {
 	})
 
 	s.Order = &order
-}
-
-func (s *SubstrateHandler) fileExists(fileSystem fs.FS, path string) bool {
-	info, err := fs.Stat(fileSystem, path)
-	if err != nil {
-		return false
-	}
-
-	if strings.HasSuffix(path, "/") {
-		return info.IsDir()
-	}
-	return !info.IsDir()
-}
-
-func (s *SubstrateHandler) findBestResource(r *http.Request) *string {
-	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
-	root := filepath.Clean(repl.ReplaceAll("{http.vars.root}", "."))
-	fsname := repl.ReplaceAll("{http.vars.fs}", "")
-	fileSystem, ok := s.fsmap.Get(fsname)
-	if !ok {
-		s.log.Error("Use of unregistered filesystem", zap.String("fs", fsname))
-		return nil
-	}
-	path := r.URL.Path
-
-	if s.fileExists(fileSystem, caddyhttp.SanitizedPathJoin(root, path)) {
-		return &path
-	}
-
-	for _, suffix := range s.Order.TryFiles {
-		bigPath := path + suffix
-		if s.fileExists(fileSystem, caddyhttp.SanitizedPathJoin(root, bigPath)) {
-			return &bigPath
-		}
-	}
-
-	return nil
-}
-
-func (s *SubstrateHandler) enableReverseProxy(r *http.Request) bool {
-	for _, ext := range s.Order.Match {
-		if strings.HasSuffix(r.URL.Path, ext) {
-			return true
-		}
-	}
-	return false
-}
-
-func (s SubstrateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	if s.Order == nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		s.log.Error("No order")
-		return nil
-	}
-
-	match := s.findBestResource(r)
-	fmt.Printf("match: %s (path %s)\n", *match, r.URL.Path)
-	if *match != r.URL.Path {
-		r.Header.Set("X-Forwarded-Path", r.URL.Path)
-		r.URL.Path = *match
-	}
-
-	if s.enableReverseProxy(r) {
-		fmt.Println("enableReverseProxy")
-		repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
-		repl.Set("substrate.host", s.Order.Host)
-	}
-
-	return next.ServeHTTP(w, r)
 }
 
 func (s *SubstrateHandler) Provision(ctx caddy.Context) error {
---
+++ middleware.go
@@ -0,0 +1,83 @@
+package substrate
+
+import (
+	"fmt"
+	"io/fs"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"go.uber.org/zap"
+)
+
+func (s *SubstrateHandler) fileExists(fileSystem fs.FS, path string) bool {
+	info, err := fs.Stat(fileSystem, path)
+	if err != nil {
+		return false
+	}
+
+	if strings.HasSuffix(path, "/") {
+		return info.IsDir()
+	}
+	return !info.IsDir()
+}
+
+func (s *SubstrateHandler) findBestResource(r *http.Request) *string {
+	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
+	root := filepath.Clean(repl.ReplaceAll("{http.vars.root}", "."))
+	fsname := repl.ReplaceAll("{http.vars.fs}", "")
+	fileSystem, ok := s.fsmap.Get(fsname)
+	if !ok {
+		s.log.Error("Use of unregistered filesystem", zap.String("fs", fsname))
+		return nil
+	}
+	path := r.URL.Path
+
+	if s.fileExists(fileSystem, caddyhttp.SanitizedPathJoin(root, path)) {
+		return &path
+	}
+
+	for _, suffix := range s.Order.TryFiles {
+		bigPath := path + suffix
+		if s.fileExists(fileSystem, caddyhttp.SanitizedPathJoin(root, bigPath)) {
+			return &bigPath
+		}
+	}
+
+	return nil
+}
+
+func (s *SubstrateHandler) enableReverseProxy(r *http.Request) bool {
+	for _, ext := range s.Order.Match {
+		if strings.HasSuffix(r.URL.Path, ext) {
+			return true
+		}
+	}
+	return false
+}
+
+func (s SubstrateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	if s.Order == nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		s.log.Error("No order")
+		return nil
+	}
+
+	match := s.findBestResource(r)
+	fmt.Printf("match: %s (path %s)\n", *match, r.URL.Path)
+	if *match != r.URL.Path {
+		r.Header.Set("X-Forwarded-Path", r.URL.Path)
+		r.URL.Path = *match
+	}
+
+	if s.enableReverseProxy(r) {
+		fmt.Println("enableReverseProxy")
+		repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
+		repl.Set("substrate.host", s.Order.Host)
+	}
+
+	return next.ServeHTTP(w, r)
+}
+
