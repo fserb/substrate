576717e83bc15a27dc071b47191be97fda7d9000
Fernando Serboncini <fserb@fserb.com.br>
2025-03-09 16:06:12 -0400

    working with 515

--- middleware.go
+++ middleware.go
@@ -1,9 +1,7 @@
 package substrate
 
 import (
-	"bufio"
 	"fmt"
-	"net"
 	"net/http"
 	"net/url"
 	"strings"
@@ -12,74 +10,17 @@ import (
 	"go.uber.org/zap"
 )
 
-// statusCodeResponseWriter is a custom http.ResponseWriter that captures the status code
-// but only buffers the response if the status code is 515.
 type statusCodeResponseWriter struct {
 	http.ResponseWriter
-	status      int
-	wroteHeader bool
-	is515       bool
+	status int
 }
 
-// WriteHeader captures the status code and determines if we should buffer the response
 func (w *statusCodeResponseWriter) WriteHeader(statusCode int) {
-	fmt.Println("WriteHeader", statusCode)
-	if !w.wroteHeader {
-		w.status = statusCode
-		w.wroteHeader = true
-
-		// Only buffer if status code is 515
-		if statusCode == 515 {
-			w.is515 = true
-			// Don't actually write the header to the underlying writer
-			return
-		}
+	w.status = statusCode
+	if statusCode == 515 {
+		return
 	}
-
-	// For all other status codes, pass through to the original writer
 	w.ResponseWriter.WriteHeader(statusCode)
-}
-
-// Write implements the http.ResponseWriter interface
-func (w *statusCodeResponseWriter) Write(b []byte) (int, error) {
-	fmt.Println("Write", string(b))
-	if !w.wroteHeader {
-		w.WriteHeader(http.StatusOK)
-	}
-
-	// If status is 515, discard the write
-	if w.is515 {
-		return len(b), nil // Pretend we wrote it
-	}
-
-	// Otherwise pass through to the original writer
-	return w.ResponseWriter.Write(b)
-}
-
-// Hijack implements the http.Hijacker interface
-func (w *statusCodeResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	fmt.Println("Hijack")
-	if hijacker, ok := w.ResponseWriter.(http.Hijacker); ok {
-		return hijacker.Hijack()
-	}
-	return nil, nil, http.ErrNotSupported
-}
-
-// Flush implements the http.Flusher interface
-func (w *statusCodeResponseWriter) Flush() {
-	fmt.Println("Flush")
-	if flusher, ok := w.ResponseWriter.(http.Flusher); ok && !w.is515 {
-		flusher.Flush()
-	}
-}
-
-// Status returns the HTTP status code
-func (w *statusCodeResponseWriter) Status() int {
-	fmt.Println("Status")
-	if !w.wroteHeader {
-		return http.StatusOK
-	}
-	return w.status
 }
 
 func (s *SubstrateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
@@ -147,16 +88,15 @@ func (s *SubstrateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request, nex
 	r.Header.Set("X-Forwarded-BaseURL", fmt.Sprintf("%s://%s", scheme, r.Host))
 
 	// Use our custom response writer to capture the status code
-	// customWriter := &statusCodeResponseWriter{ResponseWriter: w}
-	return s.proxy.ServeHTTP(w, r, next)
+	statusWriter := &statusCodeResponseWriter{ResponseWriter: w}
+	err = s.proxy.ServeHTTP(statusWriter, r, next)
 
-	// If status code is 515, reset the response and call next handler
-	// if customWriter.Status() == 515 {
-	// 	s.log.Debug("Received status 515, falling back to next handler")
-	// 	r.URL.Path = origPath
-	// 	return next.ServeHTTP(w, r)
-	// }
-	//
-	// return err
+	if statusWriter.status == 515 {
+		r.URL.Path = origPath
+		clear(w.Header())
+		return next.ServeHTTP(w, r)
+	}
+
+	return err
 }
 
