0006d064786734968767a08af4a67e38a0a65072
Fernando Serboncini <fserb@fserb.com.br>
2025-09-12 12:58:50 -0400

    test: simplify websocket e2e tests
    - Remove TestWebSocketBidirectionalCommunication test
    - Simplify message format in TestConcurrentWebSocketConnections

--- e2e/websocket_test.go
+++ e2e/websocket_test.go
@@ -68,111 +68,6 @@ Deno.serve({hostname: Deno.args[0], port: parseInt(Deno.args[1])}, (req) => {
 	}
 }
 
-func TestWebSocketBidirectionalCommunication(t *testing.T) {
-	serverBlock := `@ws_files {
-		path *.ws.js
-		file {path}
-	}
-
-	reverse_proxy @ws_files {
-		transport substrate
-		to localhost
-	}`
-
-	wsServer := `#!/usr/bin/env -S deno run --allow-net
-Deno.serve({hostname: Deno.args[0], port: parseInt(Deno.args[1])}, (req) => {
-	if (req.headers.get("upgrade") === "websocket") {
-		const { socket, response } = Deno.upgradeWebSocket(req);
-		let messageCount = 0;
-		let interval;
-
-		socket.onopen = () => {
-			socket.send("Welcome to WebSocket server!");
-
-			interval = setInterval(() => {
-				if (socket.readyState === WebSocket.OPEN) {
-					messageCount++;
-					socket.send(` + "`" + `Server message ${messageCount}` + "`" + `);
-				}
-			}, 200);
-		};
-
-		socket.onmessage = (event) => {
-			socket.send("Server received: " + event.data);
-		};
-
-		socket.onclose = () => {
-			if (interval) clearInterval(interval);
-		};
-
-		socket.onerror = (error) => {
-			if (interval) clearInterval(interval);
-		};
-
-		return response;
-	} else {
-		return new Response("WebSocket endpoint", { status: 200 });
-	}
-});`
-
-	files := []TestFile{
-		{Path: "bidirectional.ws.js", Content: wsServer, Mode: 0755},
-	}
-
-	ctx := RunE2ETest(t, serverBlock, files)
-
-	time.Sleep(100 * time.Millisecond)
-
-	wsURL := fmt.Sprintf("ws://localhost:%d/bidirectional.ws.js", ctx.HTTPPort)
-	origin := ctx.BaseURL
-
-	conn, err := websocket.Dial(wsURL, "", origin)
-	if err != nil {
-		t.Fatalf("Failed to connect to WebSocket: %v", err)
-	}
-	defer conn.Close()
-
-	var welcomeMsg string
-	if err := websocket.Message.Receive(conn, &welcomeMsg); err != nil {
-		t.Fatalf("Failed to receive welcome message: %v", err)
-	}
-
-	if welcomeMsg != "Welcome to WebSocket server!" {
-		t.Errorf("Expected welcome message, got %q", welcomeMsg)
-	}
-
-	clientMessage := "Hello from client!"
-	if err := websocket.Message.Send(conn, clientMessage); err != nil {
-		t.Fatalf("Failed to send client message: %v", err)
-	}
-
-	var echoResponse string
-	if err := websocket.Message.Receive(conn, &echoResponse); err != nil {
-		t.Fatalf("Failed to receive echo response: %v", err)
-	}
-
-	expectedEcho := "Server received: " + clientMessage
-	if echoResponse != expectedEcho {
-		t.Errorf("Expected echo %q, got %q", expectedEcho, echoResponse)
-	}
-
-	serverMessages := make([]string, 0, 3)
-	for i := 0; i < 3; i++ {
-		var serverMsg string
-		if err := websocket.Message.Receive(conn, &serverMsg); err != nil {
-			t.Fatalf("Failed to receive server message %d: %v", i+1, err)
-		}
-		serverMessages = append(serverMessages, serverMsg)
-	}
-
-	for i, msg := range serverMessages {
-		expectedMsg := fmt.Sprintf("Server message %d", i+1)
-		if msg != expectedMsg {
-			t.Errorf("Expected server message %d to be %q, got %q", i+1, expectedMsg, msg)
-		}
-	}
-}
-
 func TestConcurrentWebSocketConnections(t *testing.T) {
 	serverBlock := `@ws_files {
 		path *.ws.js
@@ -193,15 +88,13 @@ Deno.serve({hostname: Deno.args[0], port: parseInt(Deno.args[1])}, (req) => {
 
 		socket.onopen = () => {
 			connectionCount++;
-			socket.send(` + "`" + `Connection ID: ${connectionCount}` + "`" + `);
+			socket.send("ID: " + connectionCount);
 		};
 
 		socket.onmessage = (event) => {
 			const message = event.data;
-			const start = message.indexOf("Connection ") + "Connection ".length;
-			const end = message.indexOf(":");
-			const connId = start > 0 && end > start ? message.substring(start, end) : "unknown";
-			socket.send(` + "`" + `Response to connection ${connId}: ${message}` + "`" + `);
+			const [id, msg] = message.split(": ");
+			socket.send("RES: " + id + ": " + msg);
 		};
 
 		return response;
@@ -239,7 +132,7 @@ Deno.serve({hostname: Deno.args[0], port: parseInt(Deno.args[1])}, (req) => {
 		}
 
 		parts := strings.Split(connIDMsg, ": ")
-		if len(parts) != 2 || parts[0] != "Connection ID" {
+		if len(parts) != 2 || parts[0] != "ID" {
 			t.Fatalf("Unexpected connection ID format: %q", connIDMsg)
 		}
 		connectionIDs[i] = parts[1]
@@ -247,7 +140,7 @@ Deno.serve({hostname: Deno.args[0], port: parseInt(Deno.args[1])}, (req) => {
 
 	for i, conn := range connections {
 		connID := connectionIDs[i]
-		message := fmt.Sprintf("Connection %s: Hello from client %d!", connID, i+1)
+		message := fmt.Sprintf("%s: Hello from client %d!", connID, i+1)
 
 		if err := websocket.Message.Send(conn, message); err != nil {
 			t.Fatalf("Failed to send message from connection %d: %v", i+1, err)
@@ -258,7 +151,7 @@ Deno.serve({hostname: Deno.args[0], port: parseInt(Deno.args[1])}, (req) => {
 			t.Fatalf("Failed to receive response for connection %d: %v", i+1, err)
 		}
 
-		expectedResponse := fmt.Sprintf("Response to connection %s: %s", connID, message)
+		expectedResponse := fmt.Sprintf("RES: %s", message)
 		if response != expectedResponse {
 			t.Errorf("Connection %d: expected %q, got %q", i+1, expectedResponse, response)
 		}
